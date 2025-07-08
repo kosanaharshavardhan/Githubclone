06bcde61059723ed4497b7eb12167e2310e9282b
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-07-08 22:41:45 +0530

    modify css and js

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ const contents = document.querySelectorAll(".tab-content");
 
 tabs.forEach(tab => {
   tab.addEventListener("click", () => {
-    // Remove all active classes
     tabs.forEach(t => t.classList.remove("active"));
     contents.forEach(c => c.classList.add("hidden"));
 
-    // Activate clicked tab
     tab.classList.add("active");
     const tabId = tab.getAttribute("data-tab");
     document.getElementById(tabId).classList.remove("hidden");
